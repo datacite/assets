7d8521af38cec6a51a2c7e2752f34f7e155dcfab
Martin Fenner <martin.fenner@datacite.org>
2017-01-01 12:48:28 +0100

    fix test banner styling

--- source/javascripts/banner.js
+++ source/javascripts/banner.js
@@ -2,13 +2,16 @@ var banner_jquery = $.noConflict(true);
 
 (function($) {
   function init() {
+    var banner = $("<div>").attr("id", "banner");
+    $("body").prepend(banner);
+
     addTestinfo();
   }
 
   function createToolBar(id) {
     var div = $("<div>").attr("id", id);
     div.append($("<br>"));
-    $("body").prepend(div);
+    $("#banner").append(div);
     return div;
   }
 
