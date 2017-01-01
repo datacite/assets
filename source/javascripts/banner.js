1811db8eb1576c40070592f8e881faa021257b7a
Martin Fenner <martin.fenner@datacite.org>
2017-01-01 15:32:58 +0100

    further simplify banner code

--- source/javascripts/banner.js
+++ source/javascripts/banner.js
@@ -1,10 +1,6 @@
-function($) {
-  function init() {
-    var banner = $("<div>")
-      .attr("id", "banner")
-      .text("This service is for testing only.");
-    $("body").prepend(banner);
-  }
-
-  $(document).ready(init);
+function banner() {
+  var banner = $("<div>").attr("id", "banner").text("This service is for testing only.");
+  $("body").prepend(banner);
 }
+
+$(document).ready(banner);
