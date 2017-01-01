b5fe39319be11df1a7c2aa21a4f10aaa52de133a
Martin Fenner <martin.fenner@datacite.org>
2017-01-01 14:12:24 +0100

    simplify toolbar code

--- source/javascripts/banner.js
+++ source/javascripts/banner.js
@@ -8,16 +8,12 @@ var banner_jquery = $.noConflict(true);
     addTestinfo();
   }
 
-  function createToolBar(id) {
-    var div = $("<div>").attr("id", id);
-    $("#banner").append(div);
-    return div;
-  }
-
   function addTestinfo() {
-    var toolbar = createToolBar("testinfo");
-    toolbar.text("This service is for testing only.");
-    toolbar.append($("<br>"));
+    var toolbar = $("<div>")
+      .attr("id", "testinfo")
+      .text("This service is for testing only.");
+    $("#banner").append(toolbar);
+    $("#banner").append($("<br>"));
   }
 
   $(document).ready(init);
