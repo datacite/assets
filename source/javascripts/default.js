20836dda6b74702c2d094d322848afc3891303d9
Martin Fenner <martin.fenner@datacite.org>
2017-06-16 09:10:09 +0900

    added remarkable module for markdown parsing

--- source/javascripts/default.js
+++ source/javascripts/default.js
@@ -20,6 +20,7 @@
 //= require crossfilter/crossfilter.js
 //= require queue-async/queue.js
 //= require fitvids/jquery.fitvids.js
+//= require remarkable/dist/remarkable.js
 //
 // Load theme-specific components
 //= require ct-paper
