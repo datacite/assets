2338ec9370424ebaf5acdfef68d548207b9a31cf
Martin Fenner <mfenner@datacite.org>
2018-01-19 09:01:55 +0100

    include schemaorg script

---
+++ source/javascripts/schemaorg.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  var identifier = $("meta[name='DC.identifier']").attr("content");
+  if (identifier === undefined) {
+    return;
+  }
+  var doi = new URL(identifier);
+  url = 'https://data.datacite.org/application/vnd.schemaorg.ld+json/' + doi.pathname;
+
+  $.ajax({
+    url: url,
+    dataType: 'text', // don't convert JSON to Javascript object
+    success: function(data) {
+      $('<script>')
+         .attr('type', 'application/ld+json')
+         .text(data)
+         .appendTo('head');
+    },
+    error: function (error) {
+      console.log(error.responseJSON);
+    }
+  });
+});
